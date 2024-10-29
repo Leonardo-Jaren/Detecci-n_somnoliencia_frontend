c5e59446a17b79c09c6f60a9553e2001bc918dc1
Leonardo-Jaren <2022110517@udh.edu.pe>
2024-10-29 14:05:57 -0500

    Login y register diseño previo

--- SomnolienciaFrontend/src/app/app.module.ts
+++ SomnolienciaFrontend/src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+//import { PruebaComponent } from './prueba/prueba.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+//import { AuthInterceptor } from '../service/interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    LoginComponent
+//    PruebaComponent,
+    LoginComponent,
+RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+//    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- SomnolienciaFrontend/src/app/login/login.component.ts
+++ SomnolienciaFrontend/src/app/login/login.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  @ViewChild('passwordInputAccess') passwordInputAccess!: ElementRef;
+  @ViewChild('passwordEyeAccess') passwordEyeAccess!: ElementRef;
+  @ViewChild('passwordInputRegister') passwordInputRegister!: ElementRef;
+  @ViewChild('passwordEyeRegister') passwordEyeRegister!: ElementRef;
+  @ViewChild('loginAccessRegister') loginAccessRegister!: ElementRef;
 
+  togglePasswordVisibility(input: ElementRef, eyeIcon: ElementRef) {
+    const inputElement = input.nativeElement as HTMLInputElement;
+    const iconElement = eyeIcon.nativeElement as HTMLElement;
+
+    // Toggle input type
+    inputElement.type = inputElement.type === 'password' ? 'text' : 'password';
+
+    // Toggle icon classes
+    iconElement.classList.toggle('ri-eye-fill');
+    iconElement.classList.toggle('ri-eye-off-fill');
+  }
+
+  showRegisterForm() {
+    this.loginAccessRegister.nativeElement.classList.add('active');
+  }
+
+  showLoginForm() {
+    this.loginAccessRegister.nativeElement.classList.remove('active');
+  }
 }
