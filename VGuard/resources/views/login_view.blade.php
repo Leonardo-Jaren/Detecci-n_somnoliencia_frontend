483fbf82e50fff88fbb3b4087d3043a2b0fe7526
MightyCough <marcelepwi@gmail.com>
2024-11-05 01:35:45 -0500

    Inicio de Sesion con google y otras weas chanco culiao

--- VGuard/resources/views/login_view.blade.php
+++ VGuard/resources/views/login_view.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Responsive Login and Registration Form</title>
- 
+
     <!-- Remix Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.2.0/remixicon.css">
     <!-- Tailwind CSS -->
@@ -12,7 +12,7 @@
 </head>
 <body class="bg-gray-100 flex items-center justify-center min-h-screen" >
     <!-- LOGIN -->
-    <div class="mt-5 relative h-screen bg-cover bg-fixed" 
+    <div class="mt-5 relative h-screen bg-cover bg-fixed"
     style="background-image: url('log_somno_guard.png'); filter: blur(5px); opacity: 0.5;">
         <!-- Este div solo actúa como fondo -->
     </div>
@@ -24,45 +24,45 @@
             <h1 class="login__title text-6xl font-extrabold text-center m-0 text-gray-900 font-sans mb-4">Bienvenido</h1>
           <p class="text-gray-500">Nos alegra verte de nuevo con nosotros.</p>
 
-            
-            <!--h1 class="login__title text-6xl font-extrabold text-center m-0" 
-            style="color: #1C1C1C; font-family: 'Smooch Sans', 
+
+            <!--h1 class="login__title text-6xl font-extrabold text-center m-0"
+            style="color: #1C1C1C; font-family: 'Smooch Sans',
             sans-serif; transform: scaleY(1.4);">Bienvenido</h1>
 
             <p class="text-gray-500">Nos alegra verte de nuevo con nosotros.</p-->
 
-        
+
         </div>
-        
+
         <form action="" class="space-y-4">
             <div class="relative mb-4">
                 <input type="usuario" id="usuario" required placeholder=" "
-                 class="border border-gray-300 rounded-md 
+                 class="border border-gray-300 rounded-md
                 p-3 pl-10 w-full focus:outline-none focus:ring-2 focus:ring-blue-500">
                 <label for="usuario" class="absolute left-3 top-3 text-gray-500 transition-
                 transform duration-200 scale-75 -translate-y-1/2">Usuario</label>
                 <i class="ri-mail-fill absolute right-3 top-3 text-gray-500"></i>
-                
+
             </div>
 
-           
+
             <div class="relative mb-4">
-                <input type="password" id="password" required placeholder=" " 
-                class="border border-gray-300 rounded-md p-3 pl-10 w-full focus:outline-none 
+                <input type="password" id="password" required placeholder=" "
+                class="border border-gray-300 rounded-md p-3 pl-10 w-full focus:outline-none
                 focus:ring-2 focus:ring-blue-500">
                 <label for="password" class="absolute left-3 top-3 text-gray-500 transition-transform
                  duration-200 transform scale-75 -translate-y-1/2">Contraseña</label>
-                <i class="ri-lock-fill absolute right-3 top-3 text-gray-500 cursor-pointer" 
+                <i class="ri-lock-fill absolute right-3 top-3 text-gray-500 cursor-pointer"
                 id="togglePassword"></i>
             </div>
 
-            
 
-            <button type="submit" class="w-full bg-black text-white font-semibold rounded-md py-2 
+
+            <button type="submit" class="w-full bg-black text-white font-semibold rounded-md py-2
             hover:bg-blue-800 transition duration-200">Next</button>
-        
 
-            <!--button type="submit" class="w-full bg-blue-500 text-white font-semibold rounded-md 
+
+            <!--button type="submit" class="w-full bg-blue-500 text-white font-semibold rounded-md
             py-2 hover:bg-blue-600 transition duration-200">Next</button -->
         </form>
 
@@ -72,23 +72,20 @@
                 <img src="/SomnolienciaFrontend/public/icon-google.svg" alt="G" class="w-8 h-8">
             </a>
         </div-->
-    
+
 
 
         <div class="text-center mt-4">
             <p class="text-gray-500">O</p>
-            <button class="flex items-center justify-center w-full mt-2 border border-gray-300 rounded-md 
-            py-2 hover:bg-gray-100 transition duration-200">
-                <img src="icon-google.svg" 
-                alt="Google" class="w-6 h-6 mr-2">
-                <span class="text-gray-600 font-medium">Login with Google</span>
-            </button>
+            <div>
+                <a style="background-color:skyblue;" href="{{url('auth/google')}}">Login with google</a>
+            </div>
         </div>
-        
-        
+
+
 
         <p class="text-center text-gray-600 mt-4">
-            No tienes una cuenta? 
+            No tienes una cuenta?
             <a href="/register" class="text-blue-500 font-semibold">Regístrate</a>
         </p>
     </div>
--- VGuard/routes/web.php
+++ VGuard/routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GoogleController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+//Rutas para la autenticacion con Google
+route::get('auth/google',[GoogleController::class,'googlepage']);
+route::get('auth/google/callback',[GoogleController::class,'googlecallback']);
+
+// Ruta para login_view.blade.php
+Route::get('/login', function () {
+    return view('login_view'); // Cambia a 'auth.login_view' sin '.blade.php'
+})->name('login');
