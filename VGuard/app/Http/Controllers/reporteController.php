06b6324c0a48369d57c3cb748c3853e6dd7c9274
percy0216 <147108817+percy0216@users.noreply.github.com>
2024-11-05 13:46:29 -0500

    reportes 1

--- VGuard/app/Http/Controllers/reporteController.php
+++ VGuard/app/Http/Controllers/reporteController.php
@@ -15,6 +15,28 @@ class ReporteController extends Controller
 
         $selectedPeriod = $request->input('selectedPeriod', 'diario');
 
+        if($selectedPeriod == 'diario')
+        {
+            $somnolencia = 5;
+            $parpadeos = 10;
+            $cabecear = 3;
+            $bostezo = 7;
+
+        } elseif ($selectedPeriod == 'semanal')
+        {
+            $somnolencia = 25;
+            $parpadeos = 100;
+            $cabecear = 30;
+            $bostezo = 70;
+
+        } elseif($selectedPeriod == 'mensual')
+        {
+            $somnolencia = 225;
+            $parpadeos = 1000;
+            $cabecear = 333;
+            $bostezo = 745;
+        }
+
         return view('reporte', compact('somnolencia', 'parpadeos', 'cabecear', 'bostezo', 'selectedPeriod'));
     }
 }
